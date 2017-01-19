b1439f01a565cf9e9e30d13d199d61c1b8ec73a8
William Zimmermann <me@williamzimmermann.com.br>
2017-01-19 18:23:03 -0200

    Novos Módulos
    Módulo Produtos e Pedidos iniciados.

---
+++ module/Order/src/Order/Controller/OrderController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/Order for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Order\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class OrderController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return array();
+    }
+}
